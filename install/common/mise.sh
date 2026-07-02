2efc59ce3bd830e4a46116da6b5e233b58f8c460
jkomyno <12381818+jkomyno@users.noreply.github.com>
2026-07-02 17:10:03 +0400

    chore: bump mise bootstrap version

--- install/common/mise.sh
+++ install/common/mise.sh
@@ -6,7 +6,7 @@ if [[ -n "${DOTFILES_DEBUG:-}" ]]; then
   set -x
 fi
 
-readonly DEFAULT_MISE_BOOTSTRAP_VERSION="v2026.6.2"
+readonly DEFAULT_MISE_BOOTSTRAP_VERSION="v2026.6.14"
 readonly MISE_BOOTSTRAP_VERSION="${MISE_BOOTSTRAP_VERSION:-${DEFAULT_MISE_BOOTSTRAP_VERSION}}"
 readonly MISE_INSTALL_PATH="${MISE_INSTALL_PATH:-${HOME}/.local/bin/mise}"
 readonly MISE_INSTALLER_URL="https://raw.githubusercontent.com/jdx/mise/refs/tags/${MISE_BOOTSTRAP_VERSION}/packaging/standalone/install.envsubst"
