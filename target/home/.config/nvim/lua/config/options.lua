7c5d58eb82e67670ace7e7cbd676bb52605a84e0
jkomyno <12381818+jkomyno@users.noreply.github.com>
2026-07-21 16:02:54 +0400

    fix(nvim): manage custom spell dictionary

--- target/home/.config/nvim/lua/config/options.lua
+++ target/home/.config/nvim/lua/config/options.lua
@@ -11,6 +11,15 @@ vim.opt.relativenumber = true
 vim.opt.wrap = false
 vim.opt.spelllang = { "en", "it" }
 
+require("nvim.spellfile").config({ confirm = false })
+
+local custom_spellfile = vim.fn.stdpath("config") .. "/spell/custom.utf-8.add"
+vim.opt.spellfile = custom_spellfile
+
+if vim.fn.getftime(custom_spellfile) > vim.fn.getftime(custom_spellfile .. ".spl") then
+  vim.cmd("silent mkspell! " .. vim.fn.fnameescape(custom_spellfile))
+end
+
 vim.filetype.add({
   extension = {
     wat = "wat",
